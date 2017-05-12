5f6b5d1356ccc69bc9cf232e857034ee489d9f6b
matheuseduardos <matheus.eduardo7890@gmail.com>
2017-05-12 13:34:20 -0300

    Inicia implementação do detalhamento de um posto.

---
+++ app/src/main/java/com/fameg/petrolbrain_android/PlaceDetailActivity.java
@@ -0,0 +1,91 @@
+package com.fameg.petrolbrain_android;
+
+import android.app.ProgressDialog;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class PlaceDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_detail);
+
+        String placeId = getIntent().getStringExtra("PLACE_ID");
+
+        PlaceDetailFetchTask task = new PlaceDetailFetchTask();
+        task.execute(placeId);
+    }
+
+
+    private class PlaceDetailFetchTask extends AsyncTask<String, Void, JSONObject> {
+
+        @Override
+        protected void onPreExecute() {
+            ProgressDialog dialog = new ProgressDialog(PlaceDetailActivity.this);
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... strings) {
+
+            final String baseUrl = "https://maps.googleapis.com/maps/api/place/details/json?";
+
+            Uri uri = Uri.parse(baseUrl)
+                    .buildUpon()
+                    .appendQueryParameter("key", "AIzaSyD81NQ74zxczdfejdiegET7wtaPQIUmogE")
+                    .appendQueryParameter("placeid", strings[0])
+                    .build();
+
+            HttpsURLConnection conn = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(uri.toString());
+                conn = (HttpsURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.connect();
+
+                InputStream in = conn.getInputStream();
+                if(in == null) return null;
+
+                reader = new BufferedReader(new InputStreamReader(in));
+
+                StringBuffer buffer = new StringBuffer();
+
+                String linha;
+                while((linha = reader.readLine()) != null) {
+                    buffer.append(String.format("%s\n", linha));
+                }
+                if(buffer.length() == 0) return null;
+
+                Log.d("Chegou os detalhes.", buffer.toString());
+                return new JSONObject(buffer.toString());
+            } catch (IOException | JSONException e) {
+                Log.e("Pau em alguma coisa.", e.toString());
+            } finally {
+                if(conn != null) conn.disconnect();
+                if(reader != null) {
+                    try { reader.close(); }
+                    catch (IOException e) {
+                        Log.e("Erro na stream.", "Causa: ", e);
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
